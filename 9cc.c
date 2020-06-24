7cf13635a1cac4900e745c50b6e733930f7c3d22
pocari <caffelattenonsugar@gmail.com>
2020-06-25 00:42:42 +0900

    単項演算子実装

--- 9cc.c
+++ 9cc.c
@@ -161,11 +161,13 @@ Node *new_node_num(int val) {
 }
 
 // expr    = mul ("+" mul | "-" mul)*
-// mul     = primary ("*" primary | "/" primary)*
+// mul     = unary ("*" unary | "/" unary)*
+// unary   = ("+" | "-")? primary
 // primary = num | "(" expr ")"
 
 Node *expr();
 Node *mul();
+Node *unary();
 Node *primary();
 
 Node *expr() {
@@ -183,17 +185,26 @@ Node *expr() {
 }
 
 Node *mul() {
-  Node *node = primary();
+  Node *node = unary();
 
   for (;;) {
     if (consume('*')) {
-      node = new_node(ND_MUL, node, primary());
+      node = new_node(ND_MUL, node, unary());
     } else if (consume('/')) {
-      node = new_node(ND_DIV, node, primary());
+      node = new_node(ND_DIV, node, unary());
     } else {
       return node;
     }
   }
+}
+
+Node *unary() {
+  if (consume('+')) {
+    return primary();
+  } else if (consume('-')) {
+    return new_node(ND_SUB, new_node_num(0), primary());
+  }
+  return primary();
 }
 
 Node *primary() {
