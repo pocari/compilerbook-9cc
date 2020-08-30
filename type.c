323b3ae4bbf5b9ae40e325522ef12af5db237d89
pocari <caffelattenonsugar@gmail.com>
2020-08-30 14:11:10 +0900

    シフト演算の場合に型を設定してなかったので修正

--- type.c
+++ type.c
@@ -93,6 +93,8 @@ void add_type(Node *node) {
     case ND_BIT_AND:
     case ND_BIT_OR:
     case ND_BIT_XOR:
+    case ND_A_LSHIFT:
+    case ND_A_RSHIFT:
     case ND_NUM:
       node->ty = long_type;
       return;
